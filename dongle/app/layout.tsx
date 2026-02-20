eaa27360d384a21a306cd71f2a26c76ba127f80f
Francis Onukwu <onukwuf@gmail.com>
2026-02-20 22:02:40 +0100

    test: reassign to constant

--- dongle/app/layout.tsx
+++ dongle/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const intentionalError = "This variable is never used";
+const name = "Francis";
+name = "Onfranciis";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
